7a5ef488d65b6e18b46095ab3962aba52005b192
Abhishek Verma <abhishekaverma20@gmail.com>
2025-07-20 16:33:23 +0530

    minor

--- client/src/pages/MyBlogs.jsx
+++ client/src/pages/MyBlogs.jsx
@@ -255,7 +255,7 @@ const MyBlogs = () => {
 
                             {/* Action buttons */}
                             <div className="flex items-center space-x-2 ml-4">
-                                <motion.button
+                                {/* <motion.button
                                     className="p-2 text-gray-400 hover:text-blue-600 rounded-lg hover:bg-blue-50"
                                     onClick={() => handleEditClick(blog.blogId)}
                                     whileHover={{ scale: 1.05 }}
@@ -265,7 +265,7 @@ const MyBlogs = () => {
                                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                     </svg>
-                                </motion.button>
+                                </motion.button> */}
 
                                 <motion.button
                                     className="p-2 text-gray-400 hover:text-red-600 rounded-lg hover:bg-red-50"
